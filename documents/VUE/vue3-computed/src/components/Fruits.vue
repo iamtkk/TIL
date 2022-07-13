a0519fc1e2c2172671b5a26e7c1b8b0d9e0a3aa0
iamtkk <iamtkk2@gmail.com>
2022-07-13 20:51:38 +0900

    TIL/VUE3/vue3-computed

---
+++ documents/VUE/vue3-computed/src/components/Fruits.vue
@@ -0,0 +1,29 @@
+<template>
+  <section v-if="hasFruit">
+    <h1>Fruits</h1>
+    <ul>
+      <li 
+        v-for="fruit in fruits" 
+        v-bind:key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      fruits: [
+        'Apple','Banana','Cherry'
+      ]
+    }
+  },
+  computed: {
+    hasFruit(){
+      return this.fruits.length > 0
+    }
+  }
+}
+</script>

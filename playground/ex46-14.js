6456ab60479b7b513c6ed9e7fc889192bb2f03ee
iamtkk <iamtkk2@gmail.com>
2022-08-16 09:58:33 +0900

    TIL/20220816

---
+++ playground/ex46-14.js
@@ -0,0 +1,12 @@
+async function fetchTodo() {
+  // ①
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+  const response = await fetch(url);
+  const todo = await response.json();
+  console.log(todo);
+  // {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+}
+
+console.log("test");
+fetchTodo();
